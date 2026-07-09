a8d1ad7a7d9613a5026437c4b90c3b465cdbcf9d
TonicBox <73169574+Tonic-Box@users.noreply.github.com>
2026-07-09 18:45:52 -0500

    Add AssertionError's primitive-argument constructors

--- javacompat/java.lang.AssertionError.cs
+++ javacompat/java.lang.AssertionError.cs
@@ -13,6 +13,37 @@ namespace java.lang
 
         public void __init_Ljava_lang_Object__V(Object detail)
         {
+            __init_Ljava_lang_String__V(detail == null ? null : detail.toString());
+        }
+
+        public void __init_Z_V(int detail)
+        {
+            __init_Ljava_lang_String__V(String.Wrap(detail != 0 ? "true" : "false"));
+        }
+
+        public void __init_C_V(char detail)
+        {
+            __init_Ljava_lang_String__V(String.valueOf(detail));
+        }
+
+        public void __init_I_V(int detail)
+        {
+            __init_Ljava_lang_String__V(String.valueOf(detail));
+        }
+
+        public void __init_J_V(long detail)
+        {
+            __init_Ljava_lang_String__V(String.valueOf(detail));
+        }
+
+        public void __init_F_V(float detail)
+        {
+            __init_Ljava_lang_String__V(String.valueOf(detail));
+        }
+
+        public void __init_D_V(double detail)
+        {
+            __init_Ljava_lang_String__V(String.valueOf(detail));
         }
     }
 }
