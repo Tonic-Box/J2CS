c7eaa242a66763e597fbacd4403b1770480f3921
TonicBox <73169574+Tonic-Box@users.noreply.github.com>
2026-07-11 20:33:08 -0500

    Mark Random, InputStream, LinkedHashMap as extendable shim classes

--- javacompat/java.util.LinkedHashMap.cs
+++ javacompat/java.util.LinkedHashMap.cs
@@ -5,5 +5,10 @@ namespace java.util
         public LinkedHashMap(global::java.lang.RawNew r) : base(r)
         {
         }
+
+        public void __init_IFZ_V(int initialCapacity, float loadFactor, int accessOrder)
+        {
+            __init_I_V(initialCapacity);
+        }
     }
 }
